12932bf4f67700167b3d8e20f4607f039b4d45c8
Stefan Koev <st.koev@outlook.com>
2023-05-24 18:01:35 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "../src/styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header classNameName="header">
+      <div className="logo">
+        <img src="./logo.png" alt="app logo" height="68px" width="68pxs" />
+        <h1>Hello World!</h1>
+      </div>
+      <button className="btn btn-large btn-open">Share a fact</button>
+    </header>
   );
 }
 
