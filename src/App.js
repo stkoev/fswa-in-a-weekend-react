8a16d8e7ea0c6058c06d2bba4b8d8634b7c81df3
Stefan Koev <st.koev@outlook.com>
2023-05-25 21:08:03 +0300

    implement add fact

--- src/App.js
+++ src/App.js
@@ -3,19 +3,36 @@ import { useState } from "react";
 import CategoryFilter from "./components/CategoryFilter";
 import NewFactForm from "./components/NewFactForm";
 import FactList from "./components/FactList";
-import { initialFacts } from "./data";
+import { CATEGORIES, initialFacts } from "./data";
 import Header from "./components/Header";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [text, setText] = useState("");
+  const [source, setSource] = useState("");
+  const [category, setCategory] = useState("");
+  const [facts, setFacts] = useState(initialFacts);
   return (
     <>
       {/* HEADER */}
       {<Header setShowForm={setShowForm} showForm={showForm} />}
-      {showForm ? <NewFactForm /> : null}
+      {showForm ? (
+        <NewFactForm
+          categories={CATEGORIES}
+          text={text}
+          setText={setText}
+          source={source}
+          setSource={setSource}
+          category={category}
+          setCategory={setCategory}
+          setFacts={setFacts}
+          facts={facts}
+          setShowForm={setShowForm}
+        />
+      ) : null}
       <main className="main">
         <CategoryFilter />
-        <FactList facts={initialFacts} />
+        <FactList facts={facts} />
       </main>
     </>
   );
--- src/components/NewFactForm.js
+++ src/components/NewFactForm.js
@@ -1,9 +1,98 @@
 import React from "react";
 
-function NewFactForm() {
+function NewFactForm({
+  categories,
+  text,
+  setText,
+  source,
+  setSource,
+  category,
+  setCategory,
+  facts,
+  setFacts,
+  setShowForm,
+}) {
+  const renderedCategories = categories.map((cat) => {
+    return (
+      <option value={cat.name} key={cat.id + cat.color}>
+        {cat.name}
+      </option>
+    );
+  });
+
+  function isValidUrl(str) {
+    let url;
+    try {
+      url = new URL(str);
+    } catch (_) {
+      return false;
+    }
+    return url.protocol === "http:" || url.protocol === "https:";
+  }
+
+  function handleSubmit(e) {
+    // Prevent browser reload
+    e.preventDefault();
+    // 2. Check i data is valid
+    if (
+      text === "" ||
+      !isValidUrl(source) ||
+      category === "" ||
+      text.length > 200
+    ) {
+      return;
+    }
+    // 3. Create new fact object
+    const newFact = {
+      id: Math.round(Math.random() * 1000000),
+      text: text,
+      source: source,
+      category: category,
+      votesInteresting: 0,
+      votesMindblowing: 0,
+      votesFalse: 0,
+      createdIn: new Date().getFullYear(),
+    };
+    // 4 Add new fact to UI
+    setFacts([newFact, ...facts]);
+    // 5. Reset input fields
+    setText("");
+    setSource("");
+    setCategory("");
+
+    // 6. Close form
+    setShowForm((curr) => !curr);
+  }
+
   return (
-    <form action="" className="fact-form">
-      Foact form
+    <form action="" className="fact-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Share a fact with the world"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <span>{200 - text.length}</span>
+      <input
+        type="text"
+        placeholder="Trustworthy source"
+        value={source}
+        onChange={(e) => setSource(e.target.value)}
+      />
+      <select
+        name=""
+        id="select"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+      >
+        <option value="choose" defaultValue>
+          Choose category:
+        </option>
+        {renderedCategories}
+      </select>
+      <button type="submit" className="btn btn-large">
+        Post
+      </button>
     </form>
   );
 }
